91a90a5ae0fe0709cf9b7e6ed0526c472c2129d9
Abraham Sheer Dela Vega <37060537+abry-ibinex@users.noreply.github.com>
2018-05-11 14:51:50 +0800

    Update key.php

--- key.php
+++ key.php
@@ -1,3 +1,3 @@
 <?php
 
-$bot_key = "xoxb-361644715173-GfGCbugLKI73r3PJw0JM4c2L"; //Bot User OAuth Access Token from https://api.slack.com/apps/AANGABCK1/install-on-team?
+$bot_key = "PUT KEY HERE"; //Bot User OAuth Access Token from https://api.slack.com/apps/AANGABCK1/install-on-team?
