8202fd33f4a1fd6ba9a488f94bb1e526b4d8c61a
abry-ibinex <abraham.ve@ibinex.tech>
2018-09-24 13:29:58 +0800

    Added letter E to key.

--- key.sample.php
+++ key.sample.php
@@ -1,3 +1,4 @@
 <?php
 
-$bot_key = "PUT_KEY_HERE"; //Bot User OAuth Access Token from https://api.slack.com/apps/AANGABCK1/install-on-team?
+$bot_key = "PUT_KEY_HEREE"; //Bot User OAuth Access Token from 
+https://api.slack.com/apps/AANGABCK1/install-on-team?
